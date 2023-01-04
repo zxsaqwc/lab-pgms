9386dc59359ce47b403e3b91463b08e5ce4d6360
Scam2023 <95574135+zxsaqwc@users.noreply.github.com>
2023-01-04 09:55:05 +0530

    Update P6.py

--- P6.py
+++ P6.py
@@ -1,33 +1,53 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[7]:
-
-
-import numpy as np
+import pandas as pd
+from sklearn import tree
+from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import GaussianNB
+
+# Load Data from CSV
+data = pd.read_csv('tennisdata.csv')
+print("The first 5 Values of data is :\n", data.head())
+
+X = data.iloc[:, :-1]
+print("\nThe First 5 values of the train data is\n", X.head())
+
+y = data.iloc[:, -1]
+print("\nThe First 5 values of train output is\n", y.head())
+
+le_outlook = LabelEncoder()
+X.Outlook = le_outlook.fit_transform(X.Outlook)
+
+le_Temperature = LabelEncoder()
+X.Temperature = le_Temperature.fit_transform(X.Temperature)
+
+le_Humidity = LabelEncoder()
+X.Humidity = le_Humidity.fit_transform(X.Humidity)
+
+le_Windy = LabelEncoder()
+X.Windy = le_Windy.fit_transform(X.Windy)
+
+print("\nNow the Train output is\n", X.head())
+
+le_PlayTennis = LabelEncoder()
+y = le_PlayTennis.fit_transform(y)
+print("\nNow the Train output is\n",y)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.20)
+
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
 from sklearn.metrics import accuracy_score
-
-c1,c2,c3,c4 = np.loadtxt('data.csv',unpack=True,delimiter = ',')
-x= np.column_stack((c1,c3))
-y= c4
-#Create NaiveBayes Classifier
-clf = GaussianNB()
-#fit the mode
-clf.fit(x,y)
-#make predictions
-predictions = clf.predict(x)
-
-#calculate accuracy
-print(accuracy_score(y,predictions)*100)
-
+print("Accuracy is:", accuracy_score(classifier.predict(X_test), y_test))
 
 # In[ ]:
 
 
-1.0, 1.0, 1.0, 1.0
-1.0, 1.0, 1.0, 2.0
-2.0, 1.0, 1.0, 2.0
-3.0, 2.0, 1.0, 1.0
-3.0, 3.0, 2.0, 1.0
+Outlook	Temperature	Humidity	Windy	PlayTennis
+Sunny	Hot	High	Weak	No
+Sunny	Hot	High	Strong	No
+Overcast	Hot	High	Weak	Yes
+Rain	Mild	High	Weak	Yes
+Rain	Cool	Normal	Weak	Yes
 
